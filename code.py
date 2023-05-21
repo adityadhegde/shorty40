be80c222f27e9a2e4dc219a473644722ae3dcbcc
adityadhegde <aditya0.0hegde@gmail.com>
2023-05-21 20:48:41 +0530

    MORE LAYOUT EFFICIENCY MODS

--- code.py
+++ code.py
@@ -6,12 +6,18 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.extensions.stringy_keymaps import StringyKeymaps
+from kmk.modules.modtap import ModTap
+
 rollover_cols_every_rows = 4
 
 
 keyboard = KMKKeyboard()
 keyboard.extensions.append(StringyKeymaps())
 keyboard.modules.append(Layers())
+
+modtap = ModTap()
+keyboard.modules.append(modtap)
+
 keyboard.col_pins = (
     board.GP28,board.GP27,board.GP26,
     board.GP22,board.GP7,board.GP6,
@@ -22,13 +28,21 @@ keyboard.row_pins = (
     )
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
 ______ = 'NO'
+
 MOLYR = KC.MO(1)
+
+switchwinmod = KC.MT(KC.LGUI(KC.TAB),MOLYR)
+bswitchwinclt = KC.MT(KC.LGUI(KC.LSFT(KC.TAB)),KC.LCTL)
+
+switchtabgui = KC.MT(KC.LCTL(KC.TAB),KC.LGUI)
+bswitchtabsft = KC.MT(KC.LCTL(KC.LSFT(KC.TAB)),KC.LSFT)
+altesc = KC.MT( KC.ESC, KC.LALT)
 keyboard.keymap = [
     [
         "Q",'W','E','R','T','Y','U','I','O','P',
         'A','S','D','F','G','H','J','K','L','ENT',
         'Z','X','C','V','B','N','M','COMMA','DOT','QUOTE',
-        MOLYR,'RCTL','LGUI','LSFT','SPC',______,'LALT','SCOLON','MINUS','BSPC' 
+        switchwinmod,bswitchwinclt,switchtabgui,bswitchtabsft,'SPC',______,altesc,'SCOLON','MINUS','BSPC' 
     ],
     [
         "F1",'F2','F3','F4','F5','F6','F7','F8','F9','TAB',
